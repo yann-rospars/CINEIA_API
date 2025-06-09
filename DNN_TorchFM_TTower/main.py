ee0dd2fa1ee9b1b9348805f8e304656dd82caab3
yann-rospars <yann.rospars@icloud.com>
2025-06-09 17:52:59 +0200

    Ajoute le port sur le main

--- DNN_TorchFM_TTower/main.py
+++ DNN_TorchFM_TTower/main.py
@@ -74,5 +74,9 @@ def recommend_endpoint():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
